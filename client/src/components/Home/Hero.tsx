122da08dc7ea79df77acc0cd50f9d00ab5680502
Andrew Cao <andrewcao618@gmail.com>
2025-08-07 22:04:23 -0400

    Added Title Component

---
+++ client/src/components/Home/Hero.tsx
@@ -0,0 +1,21 @@
+import { Button } from "../ui/button";
+
+function Hero() {
+  return (
+    <div className="flex h-[70vh] flex-col items-center justify-center space-y-5">
+      <div className="flex flex-col items-center gap-8">
+        <h1 className="font-old-standard-tt text-center text-6xl">
+          Brand new, and exciting <br /> reads await you
+        </h1>
+        <Button
+          variant="outline"
+          className="text-md cursor-pointer bg-red-300 px-8 py-6 text-white hover:bg-red-400"
+        >
+          Sign up
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ client/src/components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Hero from "./Hero";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+    </>
+  );
+}
+export default Home;
