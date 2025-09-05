39258da58680e444ba8739960f13410a9289d9be
Andrew Cao <andrewcao618@gmail.com>
2025-09-05 13:28:10 -0400

    Reorganized header links

--- client/src/components/Home/Header.tsx
+++ client/src/components/Home/Header.tsx
@@ -10,17 +10,37 @@ const PORT = import.meta.env.VITE_PORT;
 function Header() {
   const { token, setToken } = useContext<AuthContextType>(AuthContext);
   const navigate: NavigateFunction = useNavigate();
+
+  const headerLinks: object[] = [
+    {
+      name: "All",
+      link: "/posts/all",
+    },
+    {
+      name: "Music",
+      link: "/posts/music",
+    },
+    {
+      name: "Fashion",
+      link: "/posts/fashion",
+    },
+    {
+      name: "Tech",
+      link: "/posts/tech",
+    },
+  ];
   return (
     <header className="bg-[#242424] text-white">
       <nav className="font-noto-sans flex items-center justify-between px-4 py-4 sm:px-6 md:px-8 lg:px-10">
         <h1 className="sm:text-md flex-1 text-base font-semibold lg:text-xl">
-          sayit
+          <Link to="/">sayit</Link>
         </h1>
         <ul className="flex gap-4 md:gap-5 lg:gap-7">
-          <li className="cursor-pointer">All</li>
-          <li className="cursor-pointer">Music</li>
-          <li className="cursor-pointer">Fashion</li>
-          <li className="cursor-pointer">Tech</li>
+          {headerLinks.map((item: any, index: number) => (
+            <li className="cursor-pointer hover:underline" key={index}>
+              <Link to={item.link}>{item.name}</Link>
+            </li>
+          ))}
         </ul>
         <div className="flex flex-1 justify-end space-x-3">
           {token ? (
